e8a111ffca95228b98e96164cb8aa6cacaf5d0ff
VANSHIKA <one324775@gmail.com>
2026-08-12 22:12:17 +0530

    add Aster photos update

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "www.asterhospitals.in" },
     ],
   },
 };
